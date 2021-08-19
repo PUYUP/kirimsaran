fe7b648ea00cf48f0d0f4f57740ebd0bf861ba4b
PUYUP <hellopuyup@gmail.com>
2021-08-19 16:36:04 +0700

    add path

--- config/settings/base.py
+++ config/settings/base.py
@@ -15,6 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+PROJECT_PATH = os.path.abspath(os.path.dirname(__name__))
 
 
 # Quick-start development settings - unsuitable for production
