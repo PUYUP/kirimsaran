df0d6e7ac7e0ac7bf4140744aa2afc53109c393a
PUYUP <hellopuyup@gmail.com>
2021-08-19 14:18:22 +0700

    ...

--- config/settings/development.py
+++ config/settings/development.py
@@ -33,6 +33,19 @@ DATABASES = {
     }
 }
 
+
+# Static files (CSS, JavaScript, Images)
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static/'),
+)
+
+
 # Django Debug Toolbar
 # ------------------------------------------------------------------------------
 # https://django-debug-toolbar.readthedocs.io/en/stable/installation.html
