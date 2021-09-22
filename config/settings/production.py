65b5ab8443dead5e55412bd1697bdc7df18545c2
PUYUP <hellopuyup@gmail.com>
2021-09-22 22:56:30 +0700

    change domain

--- config/settings/production.py
+++ config/settings/production.py
@@ -51,7 +51,7 @@ X_FRAME_OPTIONS = 'DENY'
 # Django csrf
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/2.2/ref/csrf/
-CSRF_COOKIE_DOMAIN = '.kirimsaran.com'
+CSRF_COOKIE_DOMAIN = '.cliptask.com'
 CSRF_HEADER_NAME = 'HTTP_X_CSRFTOKEN'
 CSRF_COOKIE_SECURE = True
 CSRF_USE_SESSIONS = False
